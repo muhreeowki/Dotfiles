17591045c32dcf48e7303ef5753d8072b41f0024
Muriuki <97036296+muhreeowki@users.noreply.github.com>
2023-11-30 11:21:31 +0300

    removed vscode file

--- vscode/extensions/uloco.theme-bluloco-light-3.7.3/replace-colors.js
+++
@@ -1,9 +0,0 @@
-const sh = require('shelljs')
-const colors = require('./colors.json')
-
-Object
-  .keys(colors.dark)
-  .forEach((name) => {
-    // sh.sed('-i', colors.dark[name], colors.light[name], './themes/bluloco-light.tmTheme')
-    sh.sed('-i', colors.dark[name], colors.light[name], './themes/bluloco-light-color-theme.json')
-  })
